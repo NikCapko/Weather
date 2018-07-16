988d1990a2223687079188225b533926e327fed2
NikCapko <nik.capko@mail.ru>
2018-07-16 15:10:30 +0300

    Change menu

--- app/src/main/java/com/example/nikolay/weather/MainActivity.java
+++ app/src/main/java/com/example/nikolay/weather/MainActivity.java
@@ -167,6 +167,7 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
                         city = input.getText().toString();
+                        getWeatherCity();
                     }
                 })
                 .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
