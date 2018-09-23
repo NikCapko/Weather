3f75ba444c5fb82d4f4b634ff4565d661f115c72
NikCapko <nik.capko@mail.ru>
2018-09-23 20:34:58 +0300

    add lang, header, swipe

--- app/src/main/java/com/example/nikolay/weather/MainActivity.java
+++ app/src/main/java/com/example/nikolay/weather/MainActivity.java
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint;
 import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
@@ -20,11 +21,20 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import butterknife.ButterKnife;
+import com.squareup.picasso.Picasso;
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
+
+import java.io.IOException;
+import java.text.DateFormat;
+import java.util.Date;
 
 import static com.example.nikolay.weather.R.layout.activity_main;
 
@@ -33,6 +43,10 @@ public class MainActivity extends AppCompatActivity {
     public static String city = "Moscow";
     final String SAVED_TEXT = "saved_text";
     public String TAG = "TAG";
+    public String BASE_URL = "http://api.openweathermap.org/data/2.5/";
+    private String LANG = "en";
+    private String UNITS = "metric";
+
     String token = Token.getToken();
 
     SharedPreferences sPref;
@@ -49,28 +63,51 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(activity_main);
+        setContentView(R.layout.activity_main);
 
         recyclerView = (RecyclerView) findViewById(R.id.recycler);
 
         loadCity();
+        LANG = getString(R.string.lang);
+
+        OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
+        httpClient.addInterceptor(new Interceptor() {
+            @Override
+            public okhttp3.Response intercept(Chain chain) throws IOException {
+                Request original = chain.request();
+
+                Request request = original.newBuilder()
+                        .header("appid", token)
+                        .header("lang", LANG)
+                        .header("units", UNITS)
+                        .method(original.method(), original.body())
+                        .build();
+
+                okhttp3.Response response = chain.proceed(request);
+
+                return response;
+            }
+        });
+
+        OkHttpClient client = httpClient.build();
 
         retrofit = new Retrofit.Builder()
-                .baseUrl("http://api.openweathermap.org/data/2.5/")
+                .baseUrl(BASE_URL)
                 .addConverterFactory(GsonConverterFactory.create())
+                .client(client)
                 .build();
 
         weatherApi = retrofit.create(WeatherApi.class);
         getWeatherCity();
     }
 
-    void getWeatherCity() {
-        Call<WeatherModel> messages = weatherApi.getWeatherCityForecast(city, token);
+    private void getWeatherCity() {
+        Call<WeatherModel> messages = weatherApi.getWeatherCityForecast(city);
         updateInfo(messages);
     }
 
-    void getWeatherCoords(Coord coordsWeather) {
-        Call<WeatherModel> messages = weatherApi.getWeatherCoordsForecast(coordsWeather.getLat(), coordsWeather.getLon(), token);
+    private void getWeatherCoords(Coord coordsWeather) {
+        Call<WeatherModel> messages = weatherApi.getWeatherCoordsForecast(coordsWeather.getLat(), coordsWeather.getLon());
         updateInfo(messages);
     }
 
@@ -191,7 +228,7 @@ public class MainActivity extends AppCompatActivity {
 
     void loadCity() {
         sPref = getPreferences(MODE_PRIVATE);
-        city = sPref.getString(SAVED_TEXT, "");
+        city = sPref.getString(SAVED_TEXT, city);
     }
 
     @Override
--- app/src/main/java/com/example/nikolay/weather/WeatherApi.java
+++ app/src/main/java/com/example/nikolay/weather/WeatherApi.java
@@ -5,12 +5,12 @@ import retrofit2.http.GET;
 import retrofit2.http.Query;
 
 public interface WeatherApi {
-    @GET("weather?units=metric&lang=ru")
-    Call<WeatherModel> getWeatherCity(@Query("q") String cityName, @Query("appid") String token);
-    @GET("weather?units=metric&lang=ru")
-    Call<WeatherModel> getWeatherCoords(@Query("lat") Double lat, @Query("lon") Double lon, @Query("appid") String token);
-    @GET("forecast?units=metric&lang=ru")
-    Call<WeatherModel> getWeatherCityForecast(@Query("q") String cityName, @Query("appid") String token);
-    @GET("forecast?units=metric&lang=ru")
-    Call<WeatherModel> getWeatherCoordsForecast(@Query("lat") Double lat, @Query("lon") Double lon, @Query("appid") String token);
+    @GET("weather")
+    Call<WeatherModel> getWeatherCity(@Query("q") String cityName);
+    @GET("weather")
+    Call<WeatherModel> getWeatherCoords(@Query("lat") Double lat, @Query("lon") Double lon);
+    @GET("forecast")
+    Call<WeatherModel> getWeatherCityForecast(@Query("q") String cityName);
+    @GET("forecast")
+    Call<WeatherModel> getWeatherCoordsForecast(@Query("lat") Double lat, @Query("lon") Double lon);
 }
